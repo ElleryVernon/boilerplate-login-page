3026f3a74eae6ee542ae8e90449c771a7b9fdef0
ElleryVernon <tmpks3@gmail.com>
2021-06-11 06:49:59 +0900

    add server

---
+++ server/models/User.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const saltRounds = 16
+const jwt = require("jsonwebtoken")
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+})
+
+userSchema.pre("save", function (next) {
+  let user = this
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화 합니다.
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err)
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function (cb) {
+  let user = this
+  // jsonwebtoken을 이용해서 Token을 생성하기
+  user.token = jwt.sign(user._id.toHexString(), "secretToken")
+  user.save((err, user) => {
+    if (err) return cb(err)
+    cb(null, user)
+  })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+  let user = this
+  // 토큰을 docode 한다.
+  jwt.verify(token, "secretToken", (err, decoded) => {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ _id: decoded, token: token }, (err, user) => {
+      if (err) return cb(Err)
+      cb(null, user)
+    })
+  })
+}
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = { User }
